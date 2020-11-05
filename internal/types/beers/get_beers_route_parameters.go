95c072b42a6127f4efa96984404919c43dcb830c
Mario Ranftl <mario.ranftl@allaboutapps.at>
2020-11-05 21:23:16 +0100

    hide non supported query params

--- internal/types/beers/get_beers_route_parameters.go
+++ internal/types/beers/get_beers_route_parameters.go
@@ -55,14 +55,6 @@ type GetBeersRouteParams struct {
 	  In: query
 	*/
 	BeerName *string `query:"beer_name"`
-	/*Returns all beers brewed after this date, the date format is mm-yyyy e.g 10-2011
-	  In: query
-	*/
-	BrewedAfter *strfmt.DateTime `query:"brewed_after"`
-	/*Returns all beers brewed before this date, the date format is mm-yyyy e.g 10-2011
-	  In: query
-	*/
-	BrewedBefore *strfmt.DateTime `query:"brewed_before"`
 	/*Returns all beers with EBC greater than the supplied number
 	  In: query
 	*/
@@ -71,14 +63,6 @@ type GetBeersRouteParams struct {
 	  In: query
 	*/
 	EbcLt *int64 `query:"ebc_lt"`
-	/*Returns all beers matching the supplied food string, this performs a fuzzy match, if you need to add spaces just add an underscore (_).
-	  In: query
-	*/
-	Food *string `query:"food"`
-	/*Returns all beers matching the supplied hops name, this performs a fuzzy match, if you need to add spaces just add an underscore (_).
-	  In: query
-	*/
-	Hops *string `query:"hops"`
 	/*Returns all beers with IBU greater than the supplied number
 	  In: query
 	*/
@@ -87,14 +71,6 @@ type GetBeersRouteParams struct {
 	  In: query
 	*/
 	IbuLt *int64 `query:"ibu_lt"`
-	/*Returns all beers matching the supplied ID's. You can pass in multiple ID's by separating them with a | symbol.
-	  In: query
-	*/
-	Ids *string `query:"ids"`
-	/*Returns all beers matching the supplied malt name, this performs a fuzzy match, if you need to add spaces just add an underscore (_).
-	  In: query
-	*/
-	Malt *string `query:"malt"`
 	/*page
 	  Minimum: 1
 	  In: query
@@ -108,10 +84,6 @@ type GetBeersRouteParams struct {
 	  Default: 25
 	*/
 	PerPage *int64 `query:"per_page"`
-	/*Returns all beers matching the supplied yeast name, this performs a fuzzy match, if you need to add spaces just add an underscore (_).
-	  In: query
-	*/
-	Yeast *string `query:"yeast"`
 }
 
 // BindRequest both binds and validates a request, it assumes that complex things implement a Validatable(strfmt.Registry) error interface
@@ -140,16 +112,6 @@ func (o *GetBeersRouteParams) BindRequest(r *http.Request, route *middleware.Mat
 		res = append(res, err)
 	}
 
-	qBrewedAfter, qhkBrewedAfter, _ := qs.GetOK("brewed_after")
-	if err := o.bindBrewedAfter(qBrewedAfter, qhkBrewedAfter, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
-	qBrewedBefore, qhkBrewedBefore, _ := qs.GetOK("brewed_before")
-	if err := o.bindBrewedBefore(qBrewedBefore, qhkBrewedBefore, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
 	qEbcGt, qhkEbcGt, _ := qs.GetOK("ebc_gt")
 	if err := o.bindEbcGt(qEbcGt, qhkEbcGt, route.Formats); err != nil {
 		res = append(res, err)
@@ -157,16 +119,6 @@ func (o *GetBeersRouteParams) BindRequest(r *http.Request, route *middleware.Mat
 
 	qEbcLt, qhkEbcLt, _ := qs.GetOK("ebc_lt")
 	if err := o.bindEbcLt(qEbcLt, qhkEbcLt, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
-	qFood, qhkFood, _ := qs.GetOK("food")
-	if err := o.bindFood(qFood, qhkFood, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
-	qHops, qhkHops, _ := qs.GetOK("hops")
-	if err := o.bindHops(qHops, qhkHops, route.Formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -180,16 +132,6 @@ func (o *GetBeersRouteParams) BindRequest(r *http.Request, route *middleware.Mat
 		res = append(res, err)
 	}
 
-	qIds, qhkIds, _ := qs.GetOK("ids")
-	if err := o.bindIds(qIds, qhkIds, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
-	qMalt, qhkMalt, _ := qs.GetOK("malt")
-	if err := o.bindMalt(qMalt, qhkMalt, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
 	qPage, qhkPage, _ := qs.GetOK("page")
 	if err := o.bindPage(qPage, qhkPage, route.Formats); err != nil {
 		res = append(res, err)
@@ -197,11 +139,6 @@ func (o *GetBeersRouteParams) BindRequest(r *http.Request, route *middleware.Mat
 
 	qPerPage, qhkPerPage, _ := qs.GetOK("per_page")
 	if err := o.bindPerPage(qPerPage, qhkPerPage, route.Formats); err != nil {
-		res = append(res, err)
-	}
-
-	qYeast, qhkYeast, _ := qs.GetOK("yeast")
-	if err := o.bindYeast(qYeast, qhkYeast, route.Formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -226,22 +163,6 @@ func (o *GetBeersRouteParams) Validate(formats strfmt.Registry) error {
 	// Required: false
 	// AllowEmptyValue: false
 
-	// brewed_after
-	// Required: false
-	// AllowEmptyValue: false
-
-	if err := o.validateBrewedAfter(formats); err != nil {
-		res = append(res, err)
-	}
-
-	// brewed_before
-	// Required: false
-	// AllowEmptyValue: false
-
-	if err := o.validateBrewedBefore(formats); err != nil {
-		res = append(res, err)
-	}
-
 	// ebc_gt
 	// Required: false
 	// AllowEmptyValue: false
@@ -250,27 +171,11 @@ func (o *GetBeersRouteParams) Validate(formats strfmt.Registry) error {
 	// Required: false
 	// AllowEmptyValue: false
 
-	// food
-	// Required: false
-	// AllowEmptyValue: false
-
-	// hops
-	// Required: false
-	// AllowEmptyValue: false
-
 	// ibu_gt
 	// Required: false
 	// AllowEmptyValue: false
 
 	// ibu_lt
-	// Required: false
-	// AllowEmptyValue: false
-
-	// ids
-	// Required: false
-	// AllowEmptyValue: false
-
-	// malt
 	// Required: false
 	// AllowEmptyValue: false
 
@@ -289,10 +194,6 @@ func (o *GetBeersRouteParams) Validate(formats strfmt.Registry) error {
 	if err := o.validatePerPage(formats); err != nil {
 		res = append(res, err)
 	}
-
-	// yeast
-	// Required: false
-	// AllowEmptyValue: false
 
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
@@ -362,88 +263,6 @@ func (o *GetBeersRouteParams) bindBeerName(rawData []string, hasKey bool, format
 	return nil
 }
 
-// bindBrewedAfter binds and validates parameter BrewedAfter from query.
-func (o *GetBeersRouteParams) bindBrewedAfter(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	// Format: date-time
-	value, err := formats.Parse("date-time", raw)
-	if err != nil {
-		return errors.InvalidType("brewed_after", "query", "strfmt.DateTime", raw)
-	}
-	o.BrewedAfter = (value.(*strfmt.DateTime))
-
-	if err := o.validateBrewedAfter(formats); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// validateBrewedAfter carries on validations for parameter BrewedAfter
-func (o *GetBeersRouteParams) validateBrewedAfter(formats strfmt.Registry) error {
-
-	// Required: false
-	if o.BrewedAfter == nil {
-		return nil
-	}
-
-	if err := validate.FormatOf("brewed_after", "query", "date-time", (*o.BrewedAfter).String(), formats); err != nil {
-		return err
-	}
-	return nil
-}
-
-// bindBrewedBefore binds and validates parameter BrewedBefore from query.
-func (o *GetBeersRouteParams) bindBrewedBefore(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	// Format: date-time
-	value, err := formats.Parse("date-time", raw)
-	if err != nil {
-		return errors.InvalidType("brewed_before", "query", "strfmt.DateTime", raw)
-	}
-	o.BrewedBefore = (value.(*strfmt.DateTime))
-
-	if err := o.validateBrewedBefore(formats); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// validateBrewedBefore carries on validations for parameter BrewedBefore
-func (o *GetBeersRouteParams) validateBrewedBefore(formats strfmt.Registry) error {
-
-	// Required: false
-	if o.BrewedBefore == nil {
-		return nil
-	}
-
-	if err := validate.FormatOf("brewed_before", "query", "date-time", (*o.BrewedBefore).String(), formats); err != nil {
-		return err
-	}
-	return nil
-}
-
 // bindEbcGt binds and validates parameter EbcGt from query.
 func (o *GetBeersRouteParams) bindEbcGt(rawData []string, hasKey bool, formats strfmt.Registry) error {
 	var raw string
@@ -488,42 +307,6 @@ func (o *GetBeersRouteParams) bindEbcLt(rawData []string, hasKey bool, formats s
 	return nil
 }
 
-// bindFood binds and validates parameter Food from query.
-func (o *GetBeersRouteParams) bindFood(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	o.Food = &raw
-
-	return nil
-}
-
-// bindHops binds and validates parameter Hops from query.
-func (o *GetBeersRouteParams) bindHops(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	o.Hops = &raw
-
-	return nil
-}
-
 // bindIbuGt binds and validates parameter IbuGt from query.
 func (o *GetBeersRouteParams) bindIbuGt(rawData []string, hasKey bool, formats strfmt.Registry) error {
 	var raw string
@@ -564,42 +347,6 @@ func (o *GetBeersRouteParams) bindIbuLt(rawData []string, hasKey bool, formats s
 		return errors.InvalidType("ibu_lt", "query", "int64", raw)
 	}
 	o.IbuLt = &value
-
-	return nil
-}
-
-// bindIds binds and validates parameter Ids from query.
-func (o *GetBeersRouteParams) bindIds(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	o.Ids = &raw
-
-	return nil
-}
-
-// bindMalt binds and validates parameter Malt from query.
-func (o *GetBeersRouteParams) bindMalt(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	o.Malt = &raw
 
 	return nil
 }
@@ -688,24 +435,6 @@ func (o *GetBeersRouteParams) validatePerPage(formats strfmt.Registry) error {
 	if err := validate.MaximumInt("per_page", "query", int64(*o.PerPage), 80, false); err != nil {
 		return err
 	}
-
-	return nil
-}
-
-// bindYeast binds and validates parameter Yeast from query.
-func (o *GetBeersRouteParams) bindYeast(rawData []string, hasKey bool, formats strfmt.Registry) error {
-	var raw string
-	if len(rawData) > 0 {
-		raw = rawData[len(rawData)-1]
-	}
-
-	// Required: false
-	// AllowEmptyValue: false
-	if raw == "" { // empty values pass all other validations
-		return nil
-	}
-
-	o.Yeast = &raw
 
 	return nil
 }
